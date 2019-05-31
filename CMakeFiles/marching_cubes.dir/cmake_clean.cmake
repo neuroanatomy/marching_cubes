9626312901b764ddacad67205c980d103abf9550
r03ert0 <rto@braincatalogue.org>
2019-05-31 10:13:37 +0200

    Initial commit

---
+++ CMakeFiles/marching_cubes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/marching_cubes.dir/main.cpp.o"
+  "marching_cubes.pdb"
+  "marching_cubes"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/marching_cubes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
